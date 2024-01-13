77ac5ab35f6ea8f9745135789f2ecfa82f316851
Jakob Strasser <97665596+JakobStrasser@users.noreply.github.com>
2024-01-13 08:31:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+
+string rootURL = @"http://books.toscrape.com/index.html";
+string downloadFolderPath = @"C:\temp\bookstoscrapecom\";
+List<string> downloadedURLs = new List<string>();
+
+Directory.CreateDirectory(downloadFolderPath);
+
+using HttpClient httpClient = new HttpClient();
+
+await downloadPage(rootURL, downloadFolderPath, httpClient, downloadedURLs);
+
+async Task downloadPage(string URL, string downloadFolderPath, HttpClient httpClient, List<string> downloadedURLs)
+{
+    //Base case
+    if (downloadedURLs.Contains(URL))
+    {
+        return;
+    }
+    else
+    { 
+        downloadedURLs.Add(URL); 
+    }
+
+    //Recursion
+
+
+
+}
